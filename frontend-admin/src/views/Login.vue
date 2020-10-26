e1a1be60665c7985604da7df20996864495e29e5
임선빈 <imtjsqls@naver.com>
2020-10-26 10:30:34 +0900

    start vue

---
+++ frontend-admin/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="container-floud">
+      <div class="row">
+        <div class="col-5">col</div>
+        <div class="col-7" style="background-color: #BBDEFB;">col</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+}
+</script>
+
+<style>
+
+</style>
