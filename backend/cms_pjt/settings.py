d293b338693f6dfac7dcdb155fb2d84eb2cc2127
김유기 <daum7766@naver.com>
2020-11-02 17:37:42 +0900

    email 선택기능으로 변경

--- backend/cms_pjt/settings.py
+++ backend/cms_pjt/settings.py
@@ -159,7 +159,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CORS_ORIGIN_ALLOW_ALL = True
 
 # 이메일 관련 환경변수
-ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_REQUIRED = False
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
