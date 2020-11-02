5e347a9ef7de981da28f9e49673aabc741d4499c
임선빈 <imtjsqls@naver.com>
2020-11-02 16:55:46 +0900

    create Search

---
+++ frontend-customer/src/views/Search/SearchEvent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container my-4">
+    <h3 class="title my-4">EVENT</h3>
+    <hr>
+
+    <div class="row my-4 ml-4">
+      <div class="col-8">
+        <img src="https://picsum.photos/600/250" class="event-img" alt="">
+      </div>
+      <div class="col-4">
+        <h4 class="event-name">이벤트 B</h4>
+        <div class="event-spec m-0">
+          <p>이벤트 설명</p>
+        </div>
+        <p class="date">기간: 2020-00-00 ~ 2020-00-00</p>
+      </div>
+    </div>
+    <hr>
+    <div class="row my-4 ml-4">
+      <div class="col-8">
+        <img src="https://picsum.photos/600/250" class="event-img" alt="">
+      </div>
+      <div class="col-4">
+        <h4 class="event-name">이벤트 A</h4>
+        <div class="event-spec m-0">
+          <p>이벤트 설명</p>
+        </div>
+        <p class="date">기간: 2020-00-00 ~ 2020-00-00</p>
+      </div>
+    </div>
+
+    <div class="button">
+      <button type="button" class="my-4 btn btn-outline-secondary btn-lg" @click="$router.go(-1)">
+        뒤로가기
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchEvent',
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend-customer/src/views/Search/SearchProduct.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container my-4">
+    <h3 class="title my-4">Product</h3>
+    <hr>
+    <div class="row col-12 my-4">
+      <div class="vertical col-12 col-sm-6 col-md-4">
+        <img class="product-img" src="@/assets/250.png" alt="">
+        <h4 class="product-name mt-2">제품명</h4>
+        <hr>
+        <h5 class="product-price">제품가격</h5>
+        <p class="product-des">간단한 설명</p>
+      </div>
+      <div class="vertical col-12 col-sm-6 col-md-4">
+        <img class="product-img" src="@/assets/250.png" alt="">
+        <h4 class="product-name mt-2">제품명</h4>
+        <hr>
+        <h5 class="product-price">제품가격</h5>
+        <p class="product-des">간단한 설명</p>
+      </div>
+      <div class="vertical col-12 col-sm-6 col-md-4">
+        <img class="product-img" src="@/assets/250.png" alt="">
+        <h4 class="product-name mt-2">제품명</h4>
+        <hr>
+        <h5 class="product-price">제품가격</h5>
+        <p class="product-des">간단한 설명</p>
+      </div>
+    </div>
+
+    <div class="button">
+      <button type="button" class="my-4 btn btn-outline-secondary btn-lg" @click="$router.go(-1)">
+        뒤로가기
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchProduct',
+
+}
+</script>
+
+<style>
+
+</style>
