90ff832af87450e5cf9800e746cd1357f28d2e13
Eunseok <eunsuk93@naver.com>
2020-11-03 09:11:57 +0900

    event layout

--- frontend-admin/src/views/homeContent/Event.vue
+++ frontend-admin/src/views/homeContent/Event.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <div class="table-container">
+      <div class="btn">
+        <button type="button" class="btn btn-outline-danger">삭제</button>
+        <button type="button" class="btn btn-outline-success">생성</button>
+      </div>
       <table class="table">
         <thead>
           <tr>
-            <th scope="col"><input type="checkbox"></th>
+            <th scope="col"><input type="checkbox" @click="checkAll(events)"></th>
             <th scope="col">카테고리</th>
             <th scope="col">제목</th>
             <th scope="col">기간</th>
@@ -14,12 +18,12 @@
         </thead>
         <tbody>
           <tr v-for="(event, index) in events" :key="index">
-            <th scope="row"><input type="checkbox"></th>
+            <th scope="row"><input type="checkbox" v-model="checked" :value="event"></th>
             <td>{{ event.category }}</td>
             <td>{{ event.title }}</td>
             <td>{{ event.startDate }} ~ {{ event.endDate }}</td>
             <td>{{ event.etc }}</td>
-            <td>isPostState</td>
+            <td>게시중</td>
           </tr>
         </tbody>
       </table>
@@ -31,6 +35,7 @@
 export default {
   data() {
     return {
+      checked: [],
       events: [
         { 
           category : "Laptop",
@@ -56,10 +61,19 @@ export default {
         ]
     }
   },
+  methods: {
+    checkAll(events) {
+      if (this.checked.length !== this.events.length){
+        for (let i=0; i<this.events.length; i++) {
+          this.checked.push(events[i])
+        }
+      } else {
+        this.checked = []
+      }
+    }
+  },
   computed: {
-    // isPostState(event) {
-    //   const today = 
-    //   if event.
+
   }
 }
 </script>
@@ -70,4 +84,10 @@ export default {
   margin-top: 10vh; 
   text-align: center;
 }
+
+.btn {
+  margin-bottom: 30px;
+  display: inline;
+}
+
 </style>
