42a71ff38fa010d33e6b7a2005614844d8c9b57a
김유기 <daum7766@naver.com>
2020-11-08 16:50:08 +0900

    시리얼라이저에 템플릿 추가, 모델변경, 메인아이템 기능 추가

--- backend/services/urls.py
+++ backend/services/urls.py
@@ -1,3 +1,4 @@
+from services.views import MainCarouselItemAPI, MainItemAPI, MainItemDetailAPI
 from django.urls import path
 from . import views
 
@@ -5,6 +6,10 @@ app_name = 'services'
 urlpatterns = [
     path('event/', views.EventList().as_view()),
     path('event/<int:pk>/', views.EventDetailAPI().as_view()),
+    path('main/', views.MainItemAPI().as_view()),
+    path('main/<int:pk>/', views.MainItemDetailAPI().as_view()),
+    path('carousel/', views.MainCarouselItemAPI().as_view()),
+    path('carousel/<int:pk/', views.MainCarouselItemDetailAPI().as_view()),
     path('notices/', views.NoticesList().as_view()),
     path('notices/<int:pk>/', views.NoticesDetail().as_view()),
     path('customer/search/', views.CustomerSearchAPI().as_view()),
