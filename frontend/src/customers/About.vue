b3e587014ce4c2838c474f74fcf9ff9302e84c8c
임선빈 <imtjsqls@naver.com>
2020-11-14 18:58:46 +0900

    create contact us

--- frontend/src/customers/About.vue
+++ frontend/src/customers/About.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <img src="@/assets/aboutus/ALCC176.jpg" alt="">
-    <img src="@/assets/aboutus/20201114_173123_1.png" alt="">
-    <img src="@/assets/aboutus/20201114_173123_2.png" alt="">
-    <img src="@/assets/aboutus/20201114_173123_3.png" alt="">
-    <img src="@/assets/aboutus/20201114_173123_4.png" alt="">
-    <img src="@/assets/aboutus/20201114_173123_5.png" alt="">
-    <img src="@/assets/aboutus/20201114_173123_6.png" alt="">
-    <img src="@/assets/aboutus/20201114_173123_7.png" alt="">
-    
+    <div class="year">
+      <img src="@/assets/aboutus/20201114_173123_1.png" alt="">
+      <img src="@/assets/aboutus/20201114_173123_2.png" alt="">
+      <img src="@/assets/aboutus/20201114_173123_3.png" alt="">
+      <img src="@/assets/aboutus/20201114_173123_4.png" alt="">
+      <img src="@/assets/aboutus/20201114_173123_5.png" alt="">
+      <img src="@/assets/aboutus/20201114_173123_6.png" alt="">
+      <img src="@/assets/aboutus/20201114_173123_7.png" alt="">
+    </div>
   </div>
 </template>
 
@@ -22,6 +23,11 @@ export default {
 
 img {
   width: 100%;
+}
+
+.year {
+  width: 80%;
+  margin: auto;
 }
 
 </style>
